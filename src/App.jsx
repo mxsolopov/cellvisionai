ec0d4137544630f9f0bc0320be940df4331eb15e
Maksim Solopov <mxsolopov@yandex.ru>
2025-05-22 10:29:44 +0300

    Update new url

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
   const [totalConfluency, setTotalConfluency] = React.useState("0.00")
 
   // const URL = "http://localhost:5000/"
-  const URL = "https://94.241.175.69/"
+  const URL = "https://healtify.ru/"
 
   // Utility function to calculate confluency
   const calculateConfluency = (maskBase64) => {
